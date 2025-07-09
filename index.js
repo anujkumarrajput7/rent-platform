1fcf3a383e5cf08f4fad40ec66e5ad5ea31a4e9e
anujsingh <asingh879785@gmail.com>
2025-07-09 09:59:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,node 
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error(err));
+
+// Routes
+app.use('/api/products', productRoutes);
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
